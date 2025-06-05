80427fbb65983e691bc95168272e7ca0e65c22ca
DinhTruong05 <dinhtruong051095@gmai.com>
2025-06-05 16:03:44 +0700

    ..

---
+++ app.java
@@ -0,0 +1,8 @@
+public class app {
+    public static void main(String[] args) {
+        int a = 10;
+        a = 100;
+        final int b = 20;
+        System.out.println("Hello World");
+    }
+}
